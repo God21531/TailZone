74bbe0c71ebcc6c127754aa7e1c4d86235b53817
God21531 <joshua21531@gmail.com>
2024-03-05 23:09:53 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <style type="text/css">
+            h5{
+                color: blue;
+            }
+            .p1{
+                text-align: justify;
+            }
+        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About us | Life Style Store</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+<body>
+<?php
+include 'includes/header.php';
+?>
+
+<div class="container" id="content">
+    <img src="img/dog20.jpg" style="float: left;  width: 200px;
+  height: 300px;
+  object-fit: cover;">
+    <div class="row"> 
+        
+        <div class="col-lg-4" align="justify">
+            <h3><mark>Unique Personalities :</mark></h3><br>
+            <p id="p1">Dogs: Known for their loyalty and affectionate nature, dogs form strong bonds with their owners. From energetic and playful to calm and cuddly, each breed brings a distinct personality.<br><br>
+Cats: Independent and mysterious, cats are skilled hunters and natural groomers. Their personalities range from aloof to social, providing a diverse range of companionship.
+            </p>
+            <h3><mark>Communication Styles:</mark></h3><br>
+            <p id="p1">Dogs: Expressive through barks, whines, and body language, dogs are excellent communicators. They often mirror their owner's emotions, creating a strong emotional connection.<br><br>
+Cats: Masters of non-verbal communication, cats use purring, meowing, and subtle body movements to convey their feelings. They appreciate quiet companionship and often choose their moments for interaction.
+            </p>
+            <h3><mark>Exercise and Play:</mark></h3><br>
+            <p id="p1">Dogs: Enthusiastic about outdoor activities, dogs thrive on exercise, be it a game of fetch or a long walk. Their boundless energy encourages an active lifestyle.<br><br>
+Cats: Playful and agile, cats enjoy interactive toys and mental stimulation. Engaging them with puzzle feeders or feather wands caters to their need for both physical and mental exercise.
+            </p>
+        </div>
+        <div class="col-lg-4" align="justify">
+            <h3><mark>Grooming Habits:</mark></h3><br>
+            <p id="p1">Dogs: Grooming routines vary among breeds, but many dogs require regular brushing, nail trims, and baths. This process enhances their well-being and strengthens the bond with their owners.<br><br>
+Cats: Fastidious groomers by nature, cats spend a significant amount of time cleaning themselves. However, certain long-haired breeds may need occasional assistance with grooming to prevent matting.
+            </p>
+            <h3><mark>Training and Obedience: </mark></h3><br>
+            <p id="p1">Dogs: Highly trainable and eager to please, dogs excel in obedience training. Positive reinforcement methods work well, fostering a cooperative relationship between owner and pet.<br><br>
+Cats: While independent, cats can learn basic commands and tricks through positive reinforcement. They respond well to treats and praise, but training sessions should be kept short and enjoyable.
+            </p>
+            <h3><mark>Health Considerations:</mark></h3><br>
+            <p id="p1">Dogs: Veterinary care, proper nutrition, and regular exercise contribute to a dog's overall health. Breed-specific health considerations should be taken into account.<br><br>
+Cats: Regular veterinary check-ups, a balanced diet, and a stimulating environment contribute to a cat's well-being. Preventive care, such as dental hygiene, is crucial for their longevity.
+            </p>
+        </div>
+    </div>
+  </div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Contact | Tail Zone</title>
+        <style type="text/css">
+            .p1{
+                text-align: justify;
+            }
+        </style>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class="container" id="content">
+    <div class="row">
+    <div class="col-lg">
+        <img src="img/contact.png" style="float: right;">
+        <h1>Get in Touch</h1>
+        <p id="p1">Hello Pet Lovers!
+
+At TailZone, we're passionate about pampering your furry friends with the finest products and services. Whether you have a question about our extensive range of pet supplies, need personalized recommendations, or just want to share adorable stories about your pets, we're here for you!
+
+Our dedicated team of pet enthusiasts is ready to assist you. From premium pet food to stylish accessories, we've got everything your pets need for a happy and healthy life. Have a specific query or looking for something unique? Let's chat!
+
+</p>
+    </div><br><br>
+    <div class="col-lg">
+        <div style="float: right;">
+            <h1>COMPANY INFORMATION</h1><br>
+            <p id="p1">Bengaluru, India - 560011</p><br>
+            <p id="p1">Phone : +91 7358503502</p><br>
+            <p id="p1">Email : support@TailZone.com</p>
+        </div>
+        <h1>CONTACT US</h1>
+        <div style="float: left;">
+        <form>
+            <div class="form-group">
+                <input type="text" name="name" placeholder="Name" autofocus="on" class="form-control" required = "true" pattern="^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$">
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" placeholder="Email" class="form-control"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required = "true">
+            </div>
+            <div class="form-group">
+                <textarea rows="5" cols="60" placeholder="Address"></textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+    </div>
+</div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
